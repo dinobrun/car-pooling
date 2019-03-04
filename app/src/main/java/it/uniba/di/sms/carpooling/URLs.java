93644a5b1f09c510425777d181dc687415385c80
nicola <n.disabato3@studenti.uniba.it>
2019-03-04 16:17:46 +0100

    fix commit ListaAutoFragment

--- app/src/main/java/it/uniba/di/sms/carpooling/URLs.java
+++ app/src/main/java/it/uniba/di/sms/carpooling/URLs.java
@@ -1,7 +1,7 @@
 package it.uniba.di.sms.carpooling;
 
 public class URLs {
-    private static final String ROOT_URL = "http://carpooling2019.altervista.org/Api.php?apicall=";
+    private static final String ROOT_URL = "https://carpooling2019.altervista.org/Api.php?apicall=";
 
     public static final String URL_REGISTER = ROOT_URL + "signup";
     public static final String URL_LOGIN= ROOT_URL + "login";
